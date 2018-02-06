f3a8ddfe55a3359b7c13e9ff9748dc25f164d575
PAOSTEPH\PAOLITA <pao_st55@hotmail.com>
2018-02-06 17:16:56 -0500

    Funcion editar mascota, y configuracion de urls

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from apps.mascota.views import index, mascota_view, mascota_list
+from apps.mascota.views import index, mascota_crear, mascota_list, mascota_edit
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'nuevo', mascota_view, name='mascota_crear'),
-    url(r'listar', mascota_list, name='mascota_listar'),
+    url(r'^nuevo', mascota_crear, name='mascota_crear'),
+    url(r'^listar/', mascota_list, name='mascota_listar'),
+    url(r'^editar/(?P<id_mascota>\d+)/$', mascota_edit, name='mascota_editar'),
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -7,12 +7,12 @@ from apps.mascota.models import Mascota
 def index(request):
     return render(request,'mascota/index.html')
 
-def mascota_view(request):
+def mascota_crear(request):
     if request.method=='POST':
         form=MascotaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('mascota:index')
+            return redirect('mascota_listar')
     else:
         form=MascotaForm()
     return render(request,'mascota/mascota_form.html',{'form':form})
@@ -21,3 +21,14 @@ def mascota_list(request):
     mascota = Mascota.objects.all()
     contexto = {'mascotas': mascota}
     return render(request, 'mascota/mascota_list.html', contexto)
+
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota_listar')
+    return render(request, 'mascota/mascota_form.html', {'form':form})
