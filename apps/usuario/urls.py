9fb77d66a308f5a47d0d7dd20342ba682c78d933
PAOSTEPH\PAOLITA <pao_st55@hotmail.com>
2018-02-19 04:30:48 -0500

    Pagina principal, y base.html

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from apps.usuario.views import RegistroUsuario, cargarLogin
+from apps.usuario.views import RegistroUsuario, principal
 from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^registrar', RegistroUsuario.as_view(), name="registrarUsu"),
+    url(r'^$', principal),
 
 ]
